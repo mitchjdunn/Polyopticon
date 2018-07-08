7522d1f7de703f5e6d1f1951792d566501a17cbb
Mitchell Dunn <mitch@mitchymitch.norwood.baki.es>
2018-07-08 12:49:09 -0400

    RPC testing

---
+++ whiteboardView/border.py
@@ -0,0 +1,99 @@
+import socket
+import cv2
+import numpy as np
+import math
+from cvHelper import cvHelper 
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+import collections
+class Border:
+
+    def __init__(self):
+        self.debug = True
+        #Final variables
+        #minimum amount of times a rectangle is found before it becomes the border
+        self.minBorderThreshold = 10
+            
+        #confirmed border corners
+        self.borderboundries = []
+        #top left corner of the rectangle.  relative coordinates are  (0,0)
+        self.origin = []
+        #slope of the top line of the rectangle.  Used for relative positioning of the LED
+        self.slope = 0
+        #rectangle dimensions
+        self.height = 0
+        self.width = 0
+        self.topRight = []
+        self.bottomLeft = []
+        #list of coordinates for corners of rectangle. counted for assurance 
+        self.potentialBorder = collections.Counter()
+
+    def findBorder(self, img):
+        modes=[cv2.RETR_EXTERNAL, cv2.RETR_LIST, cv2.RETR_CCOMP, cv2.RETR_TREE, cv2.RETR_FLOODFILL]
+        methods=[cv2.CHAIN_APPROX_NONE, cv2.CHAIN_APPROX_SIMPLE]
+        #manage contours somehow??
+        _,contours,_ = cv2.findContours(img,modes[3], methods[0])
+        rect = cv2.minAreaRect(contours[0])
+        if rect is None:
+            if self.debug:
+                print("no rectangle")
+            return
+        box = cv2.boxPoints(rect)
+        box = tuple([tuple([int(x), int(y)]) for x,y in box])
+        print(box, "Box")
+        width, height = img.shape[:2]
+        if (abs(box[0][0] - box[1][0]) > width / 4 or abs(box[0][0] - box[2][0]) > width/4) and (abs(box[0][1] - box[1][1]) > height/4 or abs(box[0][1] - box[2][1]) > height/4):
+            self.potentialBorder.update((box,))
+            print(self.potentialBorder)
+            border = self.potentialBorder.most_common(1)[0]
+            if  border[1] >= self.minBorderThreshold:
+
+                self.setBorder(border[0])
+
+    def borderFound(self):
+        mostCommon = self.potentialBorder.most_common()
+        print(mostCommon)
+        if len(mostCommon) is not 0 and mostCommon[0][1] > self.minBorderThreshold:
+            return True
+        else:
+             return False
+    
+    def setBorder(self, border):
+        self.borderboundries = border
+        print("border")
+        print(border)
+        self.origin = sorted(sorted(border)[:2], key = lambda x : x[1])[0]
+        self.topRight = sorted(sorted(border)[2:], key = lambda x : x[1])[0]
+        self.bottomLeft  = sorted(sorted(border)[:2], key = lambda x : x [1])[1]
+        self.width = math.sqrt((self.topRight[1] - self.origin[1])**2 + (self.topRight[0] - self.origin[0])**2)
+        self.height = math.sqrt((self.bottomLeft[1] - self.origin[1])**2 + (self.bottomLeft[0] - self.origin[0])**2)
+        self.slope = (self.topRight[1] - self.origin[1]) / (self.topRight[0] - self.origin[0])
+
+    def getPositionOfPoint(self, point):
+        #relative x position is intersect formula for line with a perpendicular slope
+        a = np.array([[1, -1/self.slope], [1, -self.slope]]) 
+        b = np.array([point[1] - (1/self.slope) * point[0] ,self.origin[1] - (self.slope * self.origin[0])])
+        xPos = np.linalg.solve(a,b)[1]
+        print(np.linalg.solve(a,b))
+        a = np.array([[1, -self.slope], [1, -1/self.slope]]) 
+        b = np.array([point[1]   - (self.slope) * point[0] ,self.origin[1] - ((1/self.slope) * self.origin[0])])
+        yPos = np.linalg.solve(a,b)[0]
+        print(np.linalg.solve(a,b))
+        print("origin", self.origin)
+        print("width, height", self.width, self.height)
+        print('xpos,ypos', xPos - self.origin[0],yPos - self.origin[1])
+        return [( xPos - self.origin[0]) * 100 / self.width , (yPos - self.origin[1]) * 100/ self.height ]
+
+    def inBorder(self, point):
+        p = Point(point[0], point[1])
+        polygon = Polygon((self.borderboundries[0],self.borderboundries[1],self.borderboundries[2],self.borderboundries[3]))
+        return polygon.contains(p)
+#        borderBuffer = 5
+#        topLine = (point[0] - self.origin[0]) * self.slope + self.origin[1] 
+#        bottomLine = (point[0] - self.bottomLeft[0]) *  self.slope + self.bottomLeft[1] 
+#        leftLine = (point[1] - self.origin[1] + self.origin[0] / self.slope) * self.slope
+#        rightLine = (point[1] - self.topRight[1] + self.topRight[0] / self.slope) * self.slope
+#        print("point: ", point, 'topLine: ', topLine, 'bottomLine: ', bottomLine)
+#        if point[0] > topLine + borderBuffer and point[0] < bottomLine + borderBuffer and point[1] > leftLine + borderBuffer and point[1] < rightLine - borderBuffer:
+#            return True
+#
--- whiteboardView/whiteboardView.py
+++ whiteboardView/whiteboardView.py
@@ -3,93 +3,7 @@ import cv2
 import numpy as np
 import math
 from cvHelper import cvHelper 
-import collections
-class Border:
-
-    def __init__(self):
-        self.debug = True
-        #Final variables
-        #minimum amount of times a rectangle is found before it becomes the border
-        self.minBorderThreshold = 10
-            
-        #confirmed border corners
-        self.borderboundries = []
-        #top left corner of the rectangle.  relative coordinates are  (0,0)
-        self.origin = []
-        #slope of the top line of the rectangle.  Used for relative positioning of the LED
-        self.slope = 0
-        #rectangle dimensions
-        self.height = 0
-        self.width = 0
-        self.topRight = []
-        self.bottomLeft = []
-        #list of coordinates for corners of rectangle. counted for assurance 
-        self.potentialBorder = collections.Counter()
-
-    def findBorder(self, img):
-        modes=[cv2.RETR_EXTERNAL, cv2.RETR_LIST, cv2.RETR_CCOMP, cv2.RETR_TREE, cv2.RETR_FLOODFILL]
-        methods=[cv2.CHAIN_APPROX_NONE, cv2.CHAIN_APPROX_SIMPLE]
-        #manage contours somehow??
-        _,contours,_ = cv2.findContours(img,modes[3], methods[0])
-        rect = cv2.minAreaRect(contours[0])
-        if rect is None:
-            if self.debug:
-                print("no rectangle")
-            return
-        box = cv2.boxPoints(rect)
-        box = tuple([tuple([int(x), int(y)]) for x,y in box])
-        print(box, "Box")
-        if (abs(box[0][0] - box[1][0]) > 600 or abs(box[0][0] - box[2][0]) > 600) and (abs(box[0][1] - box[1][1]) > 300 or abs(box[0][1] - box[2][1]) > 300):
-            self.potentialBorder.update((box,))
-            print(self.potentialBorder)
-            border = self.potentialBorder.most_common(1)[0]
-            if  border[1] <= self.minBorderThreshold:
-
-                self.setBorder(border[0])
-
-    def borderFound(self):
-        mostCommon = self.potentialBorder.most_common()
-        print(mostCommon)
-        if len(mostCommon) is not 0 and mostCommon[0][1] > self.minBorderThreshold:
-            return True
-        else:
-             return False
-    
-    def setBorder(self, border):
-        self.borderboundries = border
-        print("border")
-        print(border)
-        self.origin = sorted(sorted(border)[:2], key = lambda x : x[1])[0]
-        self.topRight = sorted(sorted(border)[2:], key = lambda x : x[1])[0]
-        self.bottomLeft  = sorted(sorted(border)[:2], key = lambda x : x [1])[1]
-        self.width = math.sqrt((self.topRight[1] - self.origin[1])**2 + (self.topRight[0] - self.origin[0])**2)
-        self.height = math.sqrt((self.bottomLeft[1] - self.origin[1])**2 + (self.bottomLeft[0] - self.origin[0])**2)
-        self.slope = (self.topRight[1] - self.origin[1]) / (self.topRight[0] - self.origin[0])
-
-    def getPositionOfPoint(self, point):
-        #relative x position is intersect formula for line with a perpendicular slope
-        a = np.array([[1, -1/self.slope], [1, -self.slope]]) 
-        b = np.array([point[1] - (1/self.slope) * point[0] ,self.origin[1] - (self.slope * self.origin[0])])
-        xPos = np.linalg.solve(a,b)[0][0]
-        #relative y position is intersect formul for line with a the slop as the slope        
-        a = np.array([[1, -self.slope], [1, -1/self.slope]]) 
-        b = np.array([point[1]   - (self.slope) * point[0] ,self.origin[1] - ((1/self.slope) * self.origin[0])])
-        yPos = np.linalg.solve(a,b)[0][1]
-        return [xPos / self.width, yPos / self.Height]
-
-    def inBorder(self, point):
-        borderBuffer = 5
-        topLine = (point[0] - self.origin[0]) * self.slope + self.origin[1] 
-        bottomLine = (point[0] - self.bottomLeft[0]) *  self.slope + self.bottomLeft[1] 
-        leftLine = (point[1] - self.origin[1] + self.origin[0] / self.slope) * self.slope
-        rightLine = (point[1] - self.topRight[1] + self.topRight[0] / self.slope) * self.slope
-        print("point: ", point, 'topLine: ', topLine, 'bottomLine: ', bottomLine)
-        if point[0] > topLine + borderBuffer and point[0] < bottomLine + borderBuffer and point[1] > leftLine + borderBuffer and point[1] < rightLine - borderBuffer:
-            return True
-
-        return False
-    
-
+from border import Border
 
 class Whiteboard:
 
@@ -98,19 +12,20 @@ class Whiteboard:
 
         self.border = None
         self.sock = None
-        self.debug = True
+        self.debug = False
+        self.prod = False
+        self.penDown = False
+        self.readyMessageSent = False
         
 
     def connect(self, host, port):
         self.sock = socket.socket()
         self.sock.connect((host, port))
-        return self.sock
 
-    def send(self,mesg, sock):
+    def send(self,mesg):
         self.sock.send(str.encode(mesg))
         print(mesg)
         return
-
 
     def nextFrame(self, img):
         if self.debug:
@@ -118,29 +33,54 @@ class Whiteboard:
         #CHECKING FOR BORDER
         if self.border is None:
             self.border = Border()
+            if self.prod:
+                self.send(encode('calibrating')
         if not self.border.borderFound():
             #CHANGE IMG BEFORE FINDBORDER
-            img2 = cvHelper.colorSelect2(img)
-            img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-            self.border.findBorder(img2)
-            cv2.imshow('nathin', img)
-            print(self.border.potentialBorder)
+            img1 = cvHelper.colorSelect2(img.copy())
+            img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('img1', img1)
+            self.border.findBorder(img1)
+
+            if self.debug:
+                print(self.border.potentialBorder)
             return
+
+        if not self.readyMessageSent and self.prod:
+            self.send(encode('ready')
             
+        if self.debug:
         #SHOW BORDER
-        box = np.int0(self.border.borderboundries)
-        img2 = img
-        cv2.imshow("border", cv2.drawContours(img2, [box], 0,(0,0,255), 2))
-        
+            box = np.int0(self.border.borderboundries)
+            img2 = img.copy()
+            img2 = cv2.drawContours(img2, [box], 0,(0,0,255), 2)
         
         #CHECKING FOR LED
-        img2 = cvHelper.colorSelect2(img)
-        img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-        LED = self.detectLED(img2)
-        img2 = img
+        img1 = cvHelper.colorSelect2(img.copy())
+        img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        #img2 = cv2.Canny(img2, 50, 100)
+        LED = self.detectLED(img1)
         if LED is not None:
-            cv2.imshow("LED", cv2.circle(img2,LED, 5, (255,0,0), 1))
+            if self.debug:
+                img2 = cv2.circle(img2, LED, 5, (0,255,0), -1)
+                print(self.border.getPositionOfPoint(LED))
+            if self.prod:
+                if self.penDown:
+                    self.send(str(LED[0]) + ',' + str(LED[1])
+                else:
+                    self.penDown = True
+                    self.send("down" + str(LED[0]) + ',' + str(LED[1]))
+        else:
+            if self.debug:
+                print('No LED')
+            if self.prod:
+                if self.penDown:
+                    self.penDown = False
+                    self.send('up')
 
+        if self.debug:
+            cv2.imshow('img', img)
+            cv2.imshow('img2', img2)
     def runVideo(self, videoPath):
         if self.debug:
             print("runVideo")
@@ -159,17 +99,32 @@ class Whiteboard:
         modes=[cv2.RETR_EXTERNAL, cv2.RETR_LIST, cv2.RETR_CCOMP, cv2.RETR_TREE, cv2.RETR_FLOODFILL]
         methods=[cv2.CHAIN_APPROX_NONE, cv2.CHAIN_APPROX_SIMPLE]
         #manage contours somehow??
-        _,contours,_ = cv2.findContours(img,modes[3], methods[0])
+        _,contours,_ = cv2.findContours(img,modes[1], methods[0])
         rect = cv2.minAreaRect(contours[0])
         box = cv2.boxPoints(rect)
         box = tuple([tuple(x) for x in box])
+        print('og box: ', box)
         topRight = sorted(sorted(box)[2:], key = lambda x : x[1])[0]
         bottomLeft  = sorted(sorted(box)[:2], key = lambda x : x [1])[1]
-        print(box)
-        #if self.border.inBorder(((topRight[0] + bottomLeft[0]) / 2,(topRight[1] + bottomLeft[1]) / 2)):
-        return (int((topRight[0] + bottomLeft[0]) / 2),int((topRight[1] + bottomLeft[1]) / 2))
-        return None
-        #use same method as border template.  if contours are outside current border, ignore
+        print('new box', box)
+        x,y = (int((topRight[0] + bottomLeft[0]) / 2),int((topRight[1] + bottomLeft[1]) / 2))
+        if self.border.inBorder((x,y)):
+            return (int((topRight[0] + bottomLeft[0]) / 2),int((topRight[1] + bottomLeft[1]) / 2))
 
-w = Whiteboard()
-w.runVideo('tests/piTests/vid/demotest-20s--4-full.mp4')
+def main():
+    w = Whiteboard()
+    host = "jon-laptop-win"
+    port = '1234'
+    w.debug = True
+    if w.prod:
+        connected = False
+        while not connected:
+            try:
+                w.connect()
+                connected = True
+
+            except Exception as e:
+                pass
+    w.runVideo('tests/piTests/vid/demotest-20s--4-full.mp4')
+
+if __name__ == '__main__':main()
