4996eb6789d332dbe2cdbb9a0d00acbb12a6b993
Jon Bakies <jon@baki.es>
2018-08-05 13:42:53 -0400

    moved canvas buttons into new method for prep

--- Polyopticon/whiteboard.py
+++ Polyopticon/whiteboard.py
@@ -161,27 +161,13 @@ class Paint(object):
         self.root = Tk()
         self.slavesocket = None
         self.colors = ['red', 'blue', 'white', 'green', 'purple', 'orange']
+        self.sizes = [1, 3, 5, 8, 10, 20]
+        self.currentSize = 0
 
         self.canvas = Canvas(self.root, bg='black')# , width=600, height=600)
         self.canvas.pack(fill=BOTH, expand=YES, padx = 10, pady = 10)
-
-        self.penButton = Button(self.root, text='pen', command=self.usePen, bg="dark blue", fg = "white")
-        self.penButton.configure(width = 10, height = 6, bd=0) 
-        self.canvas.create_window(10, 10, anchor=NW, window=self.penButton)
-
-        self.colorButton = Button(self.root, text='color',command=self.chooseColor, bg="dark blue", fg = "white")
-        self.colorButton.configure(width = 10, height = 6, bd = 0) 
-        self.canvas.create_window(10, 90, anchor=NW, window=self.colorButton)
-
-        self.eraserButton = Button(self.root, text='eraser', command=self.useEraser, bg="dark blue", fg = "white")
-        self.eraserButton.configure(width = 10, bd = 0, height = 6)
-        self.canvas.create_window(10, 170, anchor=NW, window=self.eraserButton)
-
-        self.sizes = [1, 3, 5, 8, 10, 20]
-        self.currentSize = 0
-        self.sizeButton = Button(self.root, text='Size (1)', command=self.changeSize, bg="dark blue", fg = "white")
-        self.sizeButton.configure(width = 10, bd = 0, height = 6)
-        self.canvas.create_window(10, 250, anchor=NW, window=self.sizeButton)
+        
+        self.addCanvasButtons()
 
         self.history = ""
 
@@ -205,6 +191,27 @@ class Paint(object):
 
             v = VideoSocket(debug=self.debug)
             threading.Thread(target = v.sendImages).start()            
+
+    def fullClearCanvas(self):
+        self.canvas.delete("all")
+
+    def addCanvasButtons(self):
+        self.penButton = Button(self.root, text='pen', command=self.usePen, bg="dark blue", fg = "white")
+        self.penButton.configure(width = 10, height = 6, bd=0) 
+        self.canvas.create_window(10, 10, anchor=NW, window=self.penButton)
+
+        self.colorButton = Button(self.root, text='color', command=self.chooseColor, bg="dark blue", fg = "white")
+        # self.colorButton = Button(self.root, text='color', command=self.fullClearCanvas, bg="dark blue", fg = "white")
+        self.colorButton.configure(width = 10, height = 6, bd = 0) 
+        self.canvas.create_window(10, 90, anchor=NW, window=self.colorButton)
+
+        self.eraserButton = Button(self.root, text='eraser', command=self.useEraser, bg="dark blue", fg = "white")
+        self.eraserButton.configure(width = 10, bd = 0, height = 6)
+        self.canvas.create_window(10, 170, anchor=NW, window=self.eraserButton)
+
+        self.sizeButton = Button(self.root, text='Size (1)', command=self.changeSize, bg="dark blue", fg = "white")
+        self.sizeButton.configure(width = 10, bd = 0, height = 6)
+        self.canvas.create_window(10, 250, anchor=NW, window=self.sizeButton)
 
     def insert64image(self, base64string):
         fh = open('temp.png', 'wb')
