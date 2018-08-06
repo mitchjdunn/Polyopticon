96c4f632e90195e3b70c9037410bbd1d5c463917
Jon Bakies <jon@baki.es>
2018-08-05 21:32:52 -0400

    more colors

--- Polyopticon/whiteboard.py
+++ Polyopticon/whiteboard.py
@@ -172,7 +172,7 @@ class Paint(object):
         self.currentColor = 0
         self.root = Tk()
         self.slavesocket = None
-        self.colors = ['red', 'blue', 'green', 'purple', 'brown', 'orangered2']
+        self.colors = ['red', 'blue', 'green', 'purple', 'brown', 'orangered4', 'firebrick4']
         self.sizes = [1, 3, 5, 8, 10, 20]
         self.currentSize = 0
 
