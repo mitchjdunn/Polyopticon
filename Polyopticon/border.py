c8326d98df75389270a29e61524db3f59876dde9
Mitchell Dunn <mitch@localhost.localdomain>
2018-08-05 23:25:31 -0400

    bad maths

--- Polyopticon/border.py
+++ Polyopticon/border.py
@@ -20,10 +20,15 @@ class Border:
         #top left corner of the rectangle.  relative coordinates are  (0,0)
         self.topLeft = []
         #slope of the top line of the rectangle.  Used for relative positioning of the LED
-        self.slope = 0.0
+        self.topslope = 0.0
+        self.bottomslope = 0.0
+        self.leftslope = 0.0
+        self.rightslope = 0.0
         #rectangle dimensions
-        self.height = 0
-        self.width = 0
+        self.leftheight = 0
+        self.rightheight = 0
+        self.topwidth = 0
+        self.bottomwidth = 0
         self.topRight = []
         self.bottomLeft = []
         self.bottomRight = []
@@ -105,28 +110,48 @@ class Border:
         #self.topRight = sorted(sorted(border)[2:], key = lambda x : x[1])[0]
         #self.bottomLeft  = sorted(sorted(border)[:2], key = lambda x : x[1])[1]
         #get width height and slope for corner points
-        self.width = math.sqrt((self.topRight[1] - self.topLeft[1])**2 + (self.topRight[0] - self.topLeft[0])**2)
-        self.height = math.sqrt((self.bottomLeft[1] - self.topLeft[1])**2 + (self.bottomLeft[0] - self.topLeft[0])**2)
+        self.topwidth = math.sqrt((self.topRight[1] - self.topLeft[1])**2 + (self.topRight[0] - self.topLeft[0])**2)
+        self.leftheight = math.sqrt((self.bottomLeft[1] - self.topLeft[1])**2 + (self.bottomLeft[0] - self.topLeft[0])**2)
+        self.bottomwidth = math.sqrt((self.bottomRight[1] - self.bottomLeft[1])**2 + (self.bottomRight[0] - self.bottomLeft[0])**2)
+        self.rightheight = math.sqrt((self.bottomRight[1] - self.topRight[1])**2 + (self.bottomRight[0] - self.topRight[0])**2)
         #rise over run baby
-        self.slope = float(self.topRight[1] - self.topLeft[1]) / (self.topRight[0] - self.topLeft[0])
+        self.topslope = float(self.topRight[1] - self.topLeft[1]) / (self.topRight[0] - self.topLeft[0])
+        self.bottomslope = float(self.bottomRight[1] - self.bottomLeft[1]) / (self.bottomRight[0] - self.bottomLeft[0])
+        self.rightslope = float(self.topRight[1] - self.bottomRight[1]) / (self.topRight[0] - self.bottomRight[0])
+        self.leftslope = float(self.topLeft[1] - self.bottomLeft[1]) / (self.topLeft[0] - self.bottomLeft[0])
 
     def getPositionOfPoint(self, point):
-        #relative x position is intersect formula for line with a perpendicular slope
-        a = np.array([[1.0, -1.0/self.slope], [1.0, float(-self.slope)]]) 
-        b = np.array([float(point[1]) - (1.0/self.slope) * float(point[0]) ,float(self.topLeft[1] - (self.slope * self.topLeft[0]))])
+        # get x position from intercept with top line
+        a = np.array([[1.0, -1.0/self.topslope], [1.0, float(-self.topslope)]]) 
+        b = np.array([float(point[1]) - (1.0/self.topslope) * float(point[0]) ,float(self.topLeft[1] - (self.topslope * self.topLeft[0]))])
         #X is equal to the x position of the intersect of the top line and a line from point with a slope perpendicular to the top line.
-        xPos = np.linalg.solve(a,b)[1]
+        xPosTop = np.linalg.solve(a,b)[1]
+         # get x position from intercept with bottom line
+        a = np.array([[1.0, -1.0/self.bottomslope], [1.0, float(-self.bottomslope)]]) 
+        b = np.array([float(point[1]) - (1.0/self.bottomslope) * float(point[0]) ,float(self.bottomLeft[1] - (self.bottomslope * self.bottomLeft[0]))])
+        xPosBottom = np.linalg.solve(a,b)[1]
+
+
+
+        #Y position
+        # get y position from intercept with left line
+        a = np.array([[1.0, float(-self.leftslope)], [1.0, -1.0/self.leftslope]]) 
+        b = np.array([float(point[1])   - float(self.leftslope) * float(point[0]) ,float(self.topLeft[1]) - ((1.0/self.leftslope) * self.topLeft[0])])
+        yPosLeft = np.linalg.solve(a,b)[0]
+        a = np.array([[1.0, float(-self.rightslope)], [1.0, -1.0/self.rightslope]]) 
+        b = np.array([float(point[1])   - float(self.rightslope) * float(point[0]) ,float(self.topRight[1]) - ((1.0/self.leftslope) * self.topRight[0])])
+        yPosRight = np.linalg.solve(a,b)[0]
+        #normalize -- subtract from the orign
+        txavg=abs(xPosTop - self.topLeft[0]) * 100 / self.topwidth
+        bxavg=abs(xPosBottom - self.bottomLeft[0]) * 100 / self.bottomwidth
+        lyavg=abs(yPosLeft - self.topLeft[1]) * 100/ self.leftheight
+        ryavg=abs(yPosRight - self.topRight[1]) * 100/ self.rightheight
         if self.debug:
-            print(np.linalg.solve(a,b))
-        a = np.array([[1.0, float(-self.slope)], [1.0, -1.0/self.slope]]) 
-        b = np.array([float(point[1])   - float(self.slope) * float(point[0]) ,float(self.topLeft[1]) - ((1.0/self.slope) * self.topLeft[0])])
-        yPos = np.linalg.solve(a,b)[0]
-        if self.debug:
-            print(np.linalg.solve(a,b))
-            print("topLeft", self.topLeft)
-            print("width, height", self.width, self.height)
-            print('xpos,ypos', xPos - self.topLeft[0],yPos - self.topLeft[1])
-        return [( xPos - self.topLeft[0]) * 100 / self.width , (yPos - self.topLeft[1]) * 100/ self.height ]
+            print('txavg:{}'.format(txavg))
+            print('bxavg:{}'.format(bxavg))
+            print('ryavg:{}'.format(ryavg))
+            print('lyavg:{}'.format(lyavg))
+        return [float(txavg + bxavg) /2, float(lyavg + ryavg) /2]
 
     def inBorder(self, point):
         p = []
@@ -138,15 +163,6 @@ class Border:
                     return False
         
         return True
-#        borderBuffer = 5
-#        topLine = (point[0] - self.topLeft[0]) * self.slope + self.topLeft[1] 
-#        bottomLine = (point[0] - self.bottomLeft[0]) *  self.slope + self.bottomLeft[1] 
-#        leftLine = (point[1] - self.topLeft[1] + self.topLeft[0] / self.slope) * self.slope
-#        rightLine = (point[1] - self.topRight[1] + self.topRight[0] / self.slope) * self.slope
-#        print("point: ", point, 'topLine: ', topLine, 'bottomLine: ', bottomLine)
-#        if point[0] > topLine + borderBuffer and point[0] < bottomLine + borderBuffer and point[1] > leftLine + borderBuffer and point[1] < rightLine - borderBuffer:
-#            return True
-#
 def main():
     from border import Border
     from whiteboardView import cvHelper
